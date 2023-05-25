6a963f8d5d2b01ea2b85994687e32514d4d98ad9
Erik Connerty <erikc@email.sc.edu>
2023-05-24 23:55:49 -0400

    refactor and torch compile for speed

--- Code/CL/MNIST_MER.sh
+++ Code/CL/MNIST_MER.sh
@@ -6,10 +6,10 @@
 #COBALT -A Performance
 #COBALT -t 5:00:00
 
-export https_proxy="https://proxy:3128"
-export http_proxy="http://proxy:3128"
-export ftp_proxy="ftp://proxy:3128"
+#export https_proxy="https://proxy:3128"
+#export http_proxy="http://proxy:3128"
+#export ftp_proxy="ftp://proxy:3128"
 
-source activate torch
-python py_run.py --save_dir "Res/mnist_MER" --opt "MER" --json_file "mnist.json" --total_runs 1
-source deactivate 
+#source activate torch
+python py_run.py --save_file "mnist_MER" --opt "MER" --json_file "mnist.json" --total_runs 1
+#source deactivate 
